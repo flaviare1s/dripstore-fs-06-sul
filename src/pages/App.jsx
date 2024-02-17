ce46d62ffa368b39dcd413597a8e193980c7216f
Flávia Reis <flaviarster@gmail.com>
2024-02-17 06:10:38 -0300

    Ajustei os imports

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,8 +1,8 @@
 import { Hero } from "../components/Hero";
 import "./App.css";
 
-function App() {
+export function App() {
   return <div className="bg-[#F5F5F5]"><Hero/></div>;
 }
 
-export default App;
+
