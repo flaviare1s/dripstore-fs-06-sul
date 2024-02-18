97d687e03814a658fe92cbd3dfa721556480a5e6
Flávia Reis <flaviarster@gmail.com>
2024-02-18 12:48:58 -0300

    Desenvolvimento dos estilos do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,48 +5,55 @@ export function Footer() {
  
     return (
         <footer className="bg-gray-900 font-Inter text-white">
-        <div className="container mx-auto py-8 px-4">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <div className="m-auto pl-5 pr-[74px] pt-[50px] pb-[46px] grid grid-cols-2 grid-rows-3 md:grid-cols-4 md:grid-rows-2 gap-3">
+
             {/* Coluna 1 */}
-            <div className="mb-4 md:mb-0">
-              <img src="logo.png" alt="Logo da Digital College" />
-              <ul>
-                <li className="mb-2">
-                  <a href="#" className="text-gray-400 hover:text-white">Link 1</a>
-                </li>
-                <li className="mb-2">
-                  <a href="#" className="text-gray-400 hover:text-white">Link 2</a>
-                </li>
-                <li className="mb-2">
-                  <a href="#" className="text-gray-400 hover:text-white">Link 3</a>
-                </li>
-                <li className="mb-2">
-                  <a href="#" className="text-gray-400 hover:text-white">Link 4</a>
-                </li>
-                <li>
-                  <a href="#" className="text-gray-400 hover:text-white">Link 5</a>
-                </li>
+            <div className="col-span-2 md:col-span-1 font-medium text-sm md:font-normal md:text-base leading-[22px] md:leading-[26px] tracking-[.25px]">
+              <img className="py-5" src="logo.png" alt="Logo da Digital College" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+              <div className="flex gap-6 pt-5 md:pt-10">
+                <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+              </div>
+            </div>
+
+            {/* Coluna 2 */}
+            <div className="col-span-1">
+              <h2 className="text-lg font-semibold leading-[22px] md:leading-[38px]">Informação</h2>
+              <ul className="flex flex-col">
+                <a href="#">Sobre Drip Store</a>
+                <a href="#">Segurança</a>
+                <a href="#">Wishlist</a>
+                <a href="#">Blog</a>
+                <a href="#">Trabalhe conosco</a>
+                <a href="#">Meus Pedidos</a>
+              </ul>
+            </div>
+
+            {/* Coluna 3 */}
+            <div className="col-span-1">
+            <h2 className="text-lg font-semibold leading-[22px] md:leading-[38px]">Categorias</h2>
+              <ul className="flex flex-col">
+                <a href="#">Camisetas</a>
+                <a href="#">Calças</a>
+                <a href="#">Bonés</a>
+                <a href="#">Headphones</a>
+                <a href="#">Tênis</a>
               </ul>
             </div>
   
-            {/* Coluna 2 e 3 */}
-            <div className="mb-4 md:mb-0 md:col-span-2 md:flex md:justify-between">
-              <div className="mb-4 md:mb-0 md:w-1/2">
-                <h2 className="text-white text-lg font-semibold mb-2">Coluna 2</h2>
-                {/* Links */}
-              </div>
-              <div className="md:w-1/2">
-                <h2 className="text-white text-lg font-semibold mb-2">Coluna 3</h2>
-                {/* Links */}
-              </div>
-            </div>
-  
             {/* Coluna 4 */}
-            <div className="mb-4 md:mb-0">
-              <h2 className="text-white text-lg font-semibold mb-2">Coluna 4</h2>
-              {/* Links */}
+            <div className="col-span-2 md:col-span-1">
+              <h2 className="text-lg font-semibold leading-[22px] md:leading-[38px]">Contato</h2>
+              <p>Av. Santos Dumont, 1510 - 1 andar - Aldeota, Fortaleza - CE, 60150-161</p>
+              <p>(85) 3051-3411</p>
             </div>
-          </div>
+            
+            {/* Linha 2 desktop */}
+            <div className="col-span-2 md:col-span-4 border-t-2 border-white border-opacity-30 pt-[23px]">
+              <p className="text-center">© 2022 Digital College</p>
+            </div>
         </div>
       </footer>
     )
