5cfc23201fddd02253808c61d69b913c2a07b89c
Elano Silva <elano_16@yahoo.com.br>
2024-02-22 12:24:36 -0300

    Adicionando o parsentInt e toFixed.

--- src/components/featuredProductList/ProductCard.jsx
+++ src/components/featuredProductList/ProductCard.jsx
@@ -6,8 +6,8 @@ export const ProductCard = ({ product, ...rest }) => {
          <br /> 
          <span className="text-[#474747] font-medium md:font-normal text-sm md:text-2xl">{product.name}</span> 
          <br /> 
-         <span className="text-[#1F1F1F] font-normal mr-3 text-base md:text-2xl line-through opacity-30">${product.price}</span>
-         <span className="text-[#1F1F1F] font-bold text-base md:text-2xl">${product.price / 2}</span>
+         <span className="text-[#1F1F1F] font-normal mr-3 text-base md:text-2xl line-through opacity-30">${parseInt(product.price)}</span>
+         <span className="text-[#1F1F1F] font-bold text-base md:text-2xl">${(product.price / 2).toFixed()}</span>
         </div>
       </a>
     )
