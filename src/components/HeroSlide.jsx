e4fb687202d07c971444594cba4e8b4c97d57184
Flávia Reis <flaviarster@gmail.com>
2024-02-23 12:52:43 -0300

    Coloquei o hover no botão

--- src/components/HeroSlide.jsx
+++ src/components/HeroSlide.jsx
@@ -12,7 +12,7 @@ export const HeroSlide = ({eyebrown, titulo, descricao, ctaText, ctaLink, imagem
         <p className='font-bold text-sm md:text-base text-[#C92071] md:text-[#F6AA1C] text-center md:text-left leading-[22px] tracking-[.75px]'>{eyebrown}</p>
         <h1 className='text-[40px] md:text-[64px] md:leading-[66px] font-extrabold text-center md:text-left text-[#1F1F1F] leading-[50px] pt-2.5 pb-5 tracking-[1px]'>{titulo}</h1>
         <p className='text-[#474747] text-sm md:text-lg font-medium leading-[22px] text-center md:text-left tracking-[.25px] md:tracking-[.75px]'>{descricao}</p>
-        <a href={ctaLink} className='bg-[#C92071] text-[#F5F5F5] text-base text-center p-3 font-bold leading-6 w-full h-12 mb-10 md:w-[220px] rounded-md mt-10 tracking-[.75px]'>{ctaText}</a>
+        <a href={ctaLink} className='bg-[#C92071] hover:bg-[#991956] text-[#F5F5F5] text-base text-center p-3 font-bold leading-6 w-full h-12 mb-10 md:w-[220px] rounded-md mt-10 tracking-[.75px]'>{ctaText}</a>
       </div>
     </div>
   )
