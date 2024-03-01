49da7b1ca0eb0112503b17ce7be37e90d31ac6e7
Edson Gustavo Santiago Silva <egustavo4@gmail.com>
2024-03-01 09:30:36 -0300

    Adjust other components to receive footer

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,13 +1,15 @@
 import { Footer } from "../components/Footer";
 import "./App.css";
-import { Header } from "../components/header/header";
+import { Header } from "../components/Header/Header";
 import { BurgerMenu } from "../components/BurgerMenu/BurgerMenu";
 
 export function App() {
   return (
     <div className="h-full min-h-screen w-full flex flex-col">
-      <Header />
-      <BurgerMenu />
+      <div className="z-40 h-[72px] lg:h-[140px]">
+        <Header />
+        <BurgerMenu />
+      </div>
       <Footer />
     </div>
   );
