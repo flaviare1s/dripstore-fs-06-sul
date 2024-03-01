93b31814de7568c6236e47e5ff6df72e4f5dd450
Flávia Reis <flaviarster@gmail.com>
2024-03-01 20:51:15 -0300

    Ajustei padding bottom

--- src/components/SmallHighlightSection/SmallHighlightSection.jsx
+++ src/components/SmallHighlightSection/SmallHighlightSection.jsx
@@ -10,32 +10,34 @@ export const SmallHighlightSection = ({ title }) => {
       <div>
         <h1 className="text-dark-gray-2 font-bold md:text-2xl md:text-center px-5">{title}</h1>
       </div>
-      <div  className="flex justify-start md:justify-center items-center gap-5 md:gap-10 p-5 overflow-x-auto">
-        <SmallHighlightItem
-          image={<TShirtIcon />}
-          name={'Camisetas'}
-          ctaLink={'#'}
-        />
-        <SmallHighlightItem
-          image={<PantsIcon />}
-          name={'Calças'}
-          ctaLink={'#'}
-        />
-        <SmallHighlightItem
-          image={<PantsIcon />}
-          name={'Bonés'}
-          ctaLink={'#'}
-        />
-        <SmallHighlightItem
-          image={<HeadphoneIcon />}
-          name={'Headphones'}
-          ctaLink={'#'}
-        />
-        <SmallHighlightItem
-          image={<TenisIcon />}
-          name={'Tênis'}
-          ctaLink={'#'}
-        />
+      <div className="pb-[60px] md:pb-[103px]">
+        <div  className="flex justify-start md:justify-center items-center gap-5 md:gap-10 p-5 overflow-x-auto">
+          <SmallHighlightItem
+            image={<TShirtIcon />}
+            name={'Camisetas'}
+            ctaLink={'#'}
+          />
+          <SmallHighlightItem
+            image={<PantsIcon />}
+            name={'Calças'}
+            ctaLink={'#'}
+          />
+          <SmallHighlightItem
+            image={<PantsIcon />}
+            name={'Bonés'}
+            ctaLink={'#'}
+          />
+          <SmallHighlightItem
+            image={<HeadphoneIcon />}
+            name={'Headphones'}
+            ctaLink={'#'}
+          />
+          <SmallHighlightItem
+            image={<TenisIcon />}
+            name={'Tênis'}
+            ctaLink={'#'}
+          />
+        </div>
       </div>
     </div>
   )
