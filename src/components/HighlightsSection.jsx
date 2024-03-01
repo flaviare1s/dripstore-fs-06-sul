53f84da8921817b3aa49a2d286df23a8869b789e
ytalobl <ytalobl@hotmail.com>
2024-03-01 12:04:09 -0300

    finished

--- src/components/HighlightsSection.jsx
+++ src/components/HighlightsSection.jsx
@@ -10,7 +10,7 @@ export function HighlightsSection() {
         <div className="border relative bg-[#D8E3F2] w-full my-[10px] rounded-lg p-5 ">
           <div className=" relative z-50 ">
             <div className="flex">
-              <p className=" bg-[#E7FF86] rounded-3xl px-[15px] py-[5px] mb-[10px]">30% OFF</p>
+              <p className="bg-[#E7FF86] rounded-3xl px-[15px] py-[5px] mb-[10px]">30% OFF</p>
             </div>
             <p className="mb-[10px]">Novo Drop Supreme</p>
             <button className="bg-white z-20 text-[#C92071] px-[20px] py-[10px] rounded-md ">Comprar</button>
