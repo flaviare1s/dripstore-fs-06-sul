7d28e3b26b210ae0481c6de2cc1354fcfc1c778c
Flávia Reis <flaviarster@gmail.com>
2024-03-01 20:30:28 -0300

    Ajustei o padding bottom para se adaptar ao componente de baixo

--- src/components/SmallHighlightItem/SmallHighlightItem.jsx
+++ src/components/SmallHighlightItem/SmallHighlightItem.jsx
@@ -6,7 +6,7 @@ export const SmallHighlightItem = ({image, name, ctaLink}) => {
             <div className="bg-white p-5 rounded-full shadow">
               <div className="min-w-[56px] aspect-square hover:text-Primary">{image}</div>
             </div>
-            <p className="text-dark-gray-2 text-xs md:text:sm font-bold leading-[22px] mt-3 tracking-[1px] md:tracking-tighter[.75px]">{name}</p>
+            <p className="text-dark-gray-2 text-xs md:text:sm font-bold leading-[22px] mt-3 tracking-[1px] md:tracking-tighter[.75px] pb-[60px] md:pb-[103px]">{name}</p>
           </div>
         </a>
       </div>
